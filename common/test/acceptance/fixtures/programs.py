aaf93bdfe38eaec0e0f4b13f4de80ac16d4e513a
Renzo Lucioni <renzo@renzolucioni.com>
2016-04-07 15:40:26 -0400

    WIP: Acceptance tests for program listing page

--- common/test/acceptance/fixtures/programs.py
+++ common/test/acceptance/fixtures/programs.py
@@ -1,12 +1,17 @@
 """
 Tools to create programs-related data for use in bok choy tests.
 """
-
+from collections import namedtuple
 import json
+
 import factory
 import requests
 
 from . import PROGRAMS_STUB_URL
+from .config import ConfigModelFixture
+
+
+FakeProgram = namedtuple('FakeProgram', ['name', 'status', 'org_key', 'course_id'])
 
 
 class Program(factory.Factory):
@@ -17,10 +22,11 @@ class Program(factory.Factory):
         model = dict
 
     id = factory.Sequence(lambda n: n)  # pylint: disable=invalid-name
-    name = "dummy-program-name"
-    subtitle = "dummy-program-subtitle"
-    category = "xseries"
-    status = "unpublished"
+    name = 'dummy-program-name'
+    subtitle = 'dummy-program-subtitle'
+    category = 'xseries'
+    status = 'unpublished'
+    marketing_slug = factory.Sequence(lambda n: 'slug-{}'.format(n))
     organizations = []
     course_codes = []
 
@@ -32,8 +38,30 @@ class Organization(factory.Factory):
     class Meta(object):
         model = dict
 
-    key = "dummyX"
-    display_name = "dummy-org-display-name"
+    key = 'dummyX'
+    display_name = 'dummy-org-display-name'
+
+
+class CourseCode(factory.Factory):
+    """
+    Factory for stubbing nested course code resources from the Programs API (v1).
+    """
+    class Meta(object):
+        model = dict
+
+    display_name = 'dummy-org-display-name'
+    run_modes = []
+
+
+class RunMode(factory.Factory):
+    """
+    Factory for stubbing nested run mode resources from the Programs API (v1).
+    """
+    class Meta(object):
+        model = dict
+
+    course_key = 'org/course/run'
+    mode_slug = 'verified'
 
 
 class ProgramsFixture(object):
@@ -41,16 +69,19 @@ class ProgramsFixture(object):
     Interface to set up mock responses from the Programs stub server.
     """
 
-    def install_programs(self, program_values):
+    def install_programs(self, fake_programs):
         """
         Sets the response data for the programs list endpoint.
 
-        At present, `program_values` needs to be a sequence of sequences of (program_name, org_key).
+        At present, `fake_programs` must be a iterable of FakeProgram named tuples.
         """
         programs = []
-        for program_name, org_key in program_values:
-            org = Organization(key=org_key)
-            program = Program(name=program_name, organizations=[org])
+        for p in fake_programs:
+            run_mode = RunMode(course_key=p.course_id)
+            course_code = CourseCode(run_modes=[run_mode])
+            org = Organization(key=p.org_key)
+
+            program = Program(name=p.name, status=p.status, organizations=[org], course_codes=[course_code])
             programs.append(program)
 
         api_result = {'results': programs}
@@ -59,3 +90,21 @@ class ProgramsFixture(object):
             '{}/set_config'.format(PROGRAMS_STUB_URL),
             data={'programs': json.dumps(api_result)},
         )
+
+
+class ProgramsConfigMixin(object):
+    """Mixin providing a method used to configure the programs feature."""
+    def set_programs_api_configuration(self, is_enabled=False, api_version=1, api_url=PROGRAMS_STUB_URL,
+                                       js_path='/js', css_path='/css'):
+        """Dynamically adjusts the Programs config model during tests."""
+        ConfigModelFixture('/config/programs', {
+            'enabled': is_enabled,
+            'enable_studio_tab': is_enabled,
+            'enable_student_dashboard': is_enabled,
+            'api_version_number': api_version,
+            'internal_service_url': api_url,
+            'public_service_url': api_url,
+            'authoring_app_js_path': js_path,
+            'authoring_app_css_path': css_path,
+            'cache_ttl': 0
+        }).install()
