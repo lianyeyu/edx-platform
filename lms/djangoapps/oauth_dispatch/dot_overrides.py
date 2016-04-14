7afcd215c3e4ea25396d35e454f45c82bc50e0b3
J. Clifford Dyer <cdyer@edx.org>
2016-04-14 20:42:16 +0000

    fixup: responding to reviews.

--- lms/djangoapps/oauth_dispatch/dot_overrides.py
+++ lms/djangoapps/oauth_dispatch/dot_overrides.py
@@ -8,8 +8,10 @@ from oauth2_provider.oauth2_validators import OAuth2Validator
 
 class EdxOAuth2Validator(OAuth2Validator):
     """
-    Validator class that mimics the default behavior, but does not require
-    users to be validated.
+    Validator class that implements edX-specific custom behavior:
+
+        * It allows users to log in with their email or username.
+        * It does not require users to be active before logging in.
     """
 
     def validate_user(self, username, password, client, request, *args, **kwargs):
@@ -28,10 +30,10 @@ class EdxOAuth2Validator(OAuth2Validator):
         Authenticate the user, allowing the user to identify themself either by
         username or email
         """
-        UserModel = get_user_model()  # pylint: disable=invalid-name
 
         authenticated_user = authenticate(username=username, password=password)
         if authenticated_user is None:
+            UserModel = get_user_model()  # pylint: disable=invalid-name
             try:
                 email_user = UserModel.objects.get(email=username)
             except UserModel.DoesNotExist:
