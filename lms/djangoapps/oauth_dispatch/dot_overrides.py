107d091cdf6bac709f2324575c379e096d22ddb1
J. Clifford Dyer <cdyer@edx.org>
2016-04-14 14:50:25 +0000

    Provide custom authenticate method that allows users to provide email (MA-2273)

--- lms/djangoapps/oauth_dispatch/dot_overrides.py
+++ lms/djangoapps/oauth_dispatch/dot_overrides.py
@@ -2,8 +2,26 @@
 Classes that override default django-oauth-toolkit behavior
 """
 
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate as django_authenticate, get_user_model
 from oauth2_provider.oauth2_validators import OAuth2Validator
+
+
+def authenticate(username, password):
+    """
+    Authenticate the user, allowing the user to identify themself either by
+    username or email
+    """
+
+    if '@' in username:
+        UserModel = get_user_model()  # pylint: disable=invalid-name
+        try:
+            user = UserModel.objects.get(email=username)
+        except UserModel.DoesNotExist:
+            return None
+        else:
+            username = user.username
+    return django_authenticate(username=username, password=password)
+
 
 class EdxOAuth2Validator(OAuth2Validator):
     """
